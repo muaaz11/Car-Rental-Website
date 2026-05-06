db2dee3354e28c8d4fec48eee40d8955bb41fbe0
muaaz11 <maazilyas11@gmail.com>
2026-05-06 06:41:02 +0500

    chagnes

--- src/components/Cards/CardContainer.jsx
+++ src/components/Cards/CardContainer.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import Card from './Card';
-import carData from './carData'; // ✅ Data Import
+import Card from "./Card";
+import carData from "./carData";
 
 function CardContainer({ limit }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 z-0">
       {carData.slice(0, limit || 3).map((card) => (
-        <Card 
-          key={card.id} 
-          id={card.id}  //  ✅ Pass the id here!  
-          images={card.images} 
+        <Card
+          key={card.id}
+          id={card.id}
+          images={card.images}
           title={card.title}
-          model={card.model} 
-          capacity={card.capacity} 
-          average={card.average} 
+          model={card.model}
+          capacity={card.capacity}
+          average={card.average}
           vehicle={card.vehicle}
           Transmission={card.Transmission}
-          price={card.price} 
+          price={card.price}
         />
-      ))} 
+      ))}
     </div>
   );
 }
